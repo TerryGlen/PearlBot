5ad8122e8d3df29a3d3364957727e8bd11a70690
TerryGlen <32306780+TerryGlen@users.noreply.github.com>
2017-09-26 16:58:39 -0500

    Comments Removal.
    Deletion of unused commented code.

--- commands/ishiatusover.js
+++ commands/ishiatusover.js
@@ -1,12 +1,7 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
     const msg = await message.channel.send("FORMULATING REPLY");
    var newmessage = ["https://i.imgur.com/sOGPFwi.png", "http://i0.kym-cdn.com/photos/images/original/000/959/042/3b7.png", "https://i.redd.it/n3upcytuojiz.png", "https://i.imgur.com/0lLTGPj.mp4", "https://i.redd.it/rz3fidl9m2hz.gif" , "https://i.redd.it/t9v1xa8hmmiy.png"];
- /*    function getMessage() {
-       return messages[Math.floor(Math.random() * messages.length)];
-    }
-    var randommessage.getMessage();
-     */
-    
+
    msg.edit(newmessage[Math.floor(Math.random() * newmessage.length)] +`\nNope`)
   };
   
