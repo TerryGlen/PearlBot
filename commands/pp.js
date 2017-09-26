19ea2f4b58e188936510c8e39e4182e26d121e9c
Terry Glinsey <saispas99@gmail.com>
2017-09-26 16:27:03 -0500

    Intergration
    Added custom function.js
    Added pearlpoints and awardpoints enmap.

--- commands/pp.js
+++ commands/pp.js
@@ -7,7 +7,7 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
     if (value <= 0) return message.reply("Invalid input detected! AWARDING OF NEGATIVE POINTS IS STRICTLY PROHIBITED!");
     if (message.mentions.users.size === 0) return message.reply("Invalid input detected! Please use link a user with @ .");
     if (message.author.id === message.mentions.users.first().id) return message.reply("Public masturbation is frowned upon in polite society.");
-    if (client.cangivepoints(client, message, value) === true) {
+    //if (client.cangivepoints(client, message, value) === true) {
         var pearlscore = client.pearlpoints.get(message.mentions.users.first().id) || { points: 0, level: 0 };
         //Add value to current points
         pearlscore.points += value;
@@ -16,11 +16,11 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
         const pearlface = client.emojis.find("name", "pearlpoint")
         message.channel.send(`${pearlface} Awarded ` + value + ' Pearl-Points to ' + message.mentions.users.first().username);
 
-    } else {
-        return
-    }
+    // } else {
+    //     return
+    // }
 
-    //if(!client.cangivepoints(client,message,value)) return message.reply("False");
+   
 
 };
 
